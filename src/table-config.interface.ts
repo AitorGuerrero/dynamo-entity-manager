9a129a6f8620156ad5495ea20c7d33786d6cc544
Aitor Guerrero <aguerrerolsasarte@gmail.com>
2019-01-07 19:51:53 +0100

    table config key schema range optional

--- src/table-config.interface.ts
+++ src/table-config.interface.ts
@@ -2,7 +2,7 @@ import {DynamoDB} from "aws-sdk";
 
 export interface ITableConfig<Entity> {
 	tableName: string;
-	keySchema: {hash: string, range: string};
+	keySchema: {hash: string, range?: string};
 	marshal?: (e: Entity) => DynamoDB.DocumentClient.AttributeMap;
 	versionKey?: string;
 }
